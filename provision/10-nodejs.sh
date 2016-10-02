9459ad5c73bf0d1166bdfeb94564f222ba8fe2ce
leny <info@flatland.be>
2016-10-02 16:57:32 +0200

    Remove comments in nodejs install script

--- provision/10-nodejs.sh
+++ provision/10-nodejs.sh
@@ -5,9 +5,7 @@
 # coded by leny@flatLand!
 # started at 02/09/2016
 
-# ---------- Install node
 curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
 sudo apt-get install -y nodejs
 
-# ---------- Install global npm packages
 sudo npm install -g gulp grunt node-inspector forever

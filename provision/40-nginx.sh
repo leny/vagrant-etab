d1f9c5ea0a86e75c67f6c162e91b4aad3120a43c
leny <info@flatland.be>
2016-11-19 20:09:09 +0100

    Add self-signed ssl support to nginx

--- provision/40-nginx.sh
+++ provision/40-nginx.sh
@@ -9,4 +9,10 @@ apt-get install -y nginx-extras
 cp /tmp/nginx/nginx.conf /etc/nginx/nginx.conf
 cp /tmp/nginx/host.conf /etc/nginx/sites-available/default
 
+mkdir /etc/nginx/ssl
+echo "Generate self-signed certificate (this can take time)…"
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -subj "/C=BE/L=Liège/O=flatLand\!/CN=flatland.be" -keyout /etc/nginx/ssl/nginx.key -out /etc/nginx/ssl/nginx.crt 2> /dev/null
+openssl dhparam -out /etc/nginx/ssl/dhparam.pem 2048 2> /dev/null
+echo "…done."
+
 service nginx restart
